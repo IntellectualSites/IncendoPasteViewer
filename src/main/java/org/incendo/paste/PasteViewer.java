f7aa54e82e0b78b20d432581409733ccb1d0e857
Sauilitired <sauilitired@gmail.com>
2019-01-09 16:11:03 +0100

    Stuffz and things

--- src/main/java/org/incendo/paste/PasteViewer.java
+++ src/main/java/org/incendo/paste/PasteViewer.java
@@ -5,7 +5,7 @@
  *    | . \  \ V /| (_| || | | || |_ | |_| || | | | | |
  *    |_|\_\  \_/  \__,_||_| |_| \__| \__,_||_| |_| |_|
  *
- *    Copyright (C) 2018 Alexander Söderberg
+ *    Copyright (C) 2019 Alexander Söderberg
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,19 +21,37 @@
  */
 package org.incendo.paste;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import xyz.kvantum.server.api.config.CoreConfig;
 import xyz.kvantum.server.api.core.Kvantum;
+import xyz.kvantum.server.api.core.ServerImplementation;
+import xyz.kvantum.server.api.logging.Logger;
+import xyz.kvantum.server.api.request.AbstractRequest;
+import xyz.kvantum.server.api.response.Response;
+import xyz.kvantum.server.api.util.CollectionUtil;
 import xyz.kvantum.server.api.util.RequestManager;
+import xyz.kvantum.server.api.views.annotatedviews.ViewMatcher;
 import xyz.kvantum.server.implementation.DefaultLogWrapper;
 import xyz.kvantum.server.implementation.ServerContext;
 import xyz.kvantum.server.implementation.StandaloneServer;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 
 /**
  * Launcher and main class
  */
-public class PasteViewer {
+public final class PasteViewer {
 
     public static void main(final String[] args) {
         // Because Java is stupid...
@@ -42,6 +60,13 @@ public class PasteViewer {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        new PasteViewer();
+    }
+
+    private final JSONParser jsonParser = new JSONParser();
+    private final File pasteFolder;
+
+    private PasteViewer() {
         // Create server context
         final ServerContext serverContext = ServerContext.builder().standalone(true)
             .logWrapper(new DefaultLogWrapper())
@@ -49,7 +74,71 @@ public class PasteViewer {
             .serverSupplier(StandaloneServer::new)
             .router(RequestManager.builder().build()).build();
         final Optional<Kvantum> serverOptional = serverContext.create();
-        serverOptional.ifPresent(Kvantum::start);
+        this.pasteFolder = new File(serverContext.getCoreFolder(), "pastes");
+        serverOptional.ifPresent(server -> {
+            server.getRouter().scanAndAdd(this);
+            server.getRouter().add(new PasteRestService(server.getCoreFolder()));
+            // Otherwise shit doesn't work when running through gradle
+            CoreConfig.enableInputThread = false;
+            server.start();
+        });
+    }
+
+    @NotNull private Paste getPaste(@NotNull final String id) {
+        File file;
+        if (!(file = new File(pasteFolder, String.format("%s.json", id))).exists()) {
+            Logger.error("Unknown paste ID requested: {}", id);
+            return new Paste("", "", Collections.emptyList(), Collections.emptyList());
+        }
+        JSONObject object;
+        try (final Reader reader = new FileReader(file)){
+            object = (JSONObject) jsonParser.parse(reader);
+        } catch (final Throwable throwable) {
+            throwable.printStackTrace();
+            Logger.error("Couldn't parse paste with ID {}", id);
+            return new Paste("", "", Collections.emptyList(), Collections.emptyList());
+        }
+        if (object == null) {
+            Logger.error("Couldn't create JSON object for paste with ID {}", id);
+            return new Paste("", "", Collections.emptyList(), Collections.emptyList());
+        }
+
+        final String time = object.getOrDefault("created", "").toString();
+        final JSONObject jsonFiles = (JSONObject) object.get("files");
+        final JSONArray jsonFileNames = (JSONArray) object.get("file_names");
+        final Collection<String> file_targets = new ArrayList<>(), file_content = new ArrayList<>();
+        int index = 0;
+        boolean first = true;
+        for (final Object fileName : jsonFileNames) {
+            final int currentIndex = index++;
+            file_targets.add(String.format("<li %s><a data-target='#content-%d'>%s</a></li>",
+                first ? "class='active'" : "", currentIndex, fileName.toString()));
+            file_content.add(String.format("<div id='content-%d'><pre><code>%s</code></pre>", currentIndex,
+                jsonFiles.get(fileName.toString()).toString()));
+            if (first)  {
+                first = false;
+            }
+        }
+        return new Paste(id, time, file_targets, file_content);
+    }
+
+    @ViewMatcher(filter = "view/<paste>", name = "incendo-paste-main", cache = false)
+    public void servePaste(final AbstractRequest request, final Response response) {
+        final String pasteId = getNullable(request.get("paste"));
+        final Paste paste = getPaste(pasteId);
+        String fileContent = ServerImplementation.getImplementation().getFileSystem().getPath("/templates/paste_view.html").readFile();
+        // Hacky, because I couldn't get kvantum to work :p
+        fileContent = fileContent.replace("{paste_id}", paste.getId()).replace("{paste_time}", paste.getTime())
+            .replace("{file_list}", CollectionUtil.join(paste.getFile_targets(), "\n"))
+            .replace("{file_content}", CollectionUtil.join(paste.getFile_content(), "\n"));
+        response.setResponse(fileContent.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @NotNull private static String getNullable(@Nullable final Object object) {
+        if (object == null) {
+            return "";
+        }
+        return object.toString();
     }
 
 }
---
+++ src/main/java/org/incendo/paste/rest/UploadService.java
@@ -0,0 +1,158 @@
+/*
+ *    _  __                     _
+ *    | |/ /__   __ __ _  _ __  | |_  _   _  _ __ ___
+ *    | ' / \ \ / // _` || '_ \ | __|| | | || '_ ` _ \
+ *    | . \  \ V /| (_| || | | || |_ | |_| || | | | | |
+ *    |_|\_\  \_/  \__,_||_| |_| \__| \__,_||_| |_| |_|
+ *
+ *    Copyright (C) 2019 Alexander Söderberg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.incendo.paste.rest;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import xyz.kvantum.server.api.matching.ViewPattern;
+import xyz.kvantum.server.api.request.AbstractRequest;
+import xyz.kvantum.server.api.request.HttpMethod;
+import xyz.kvantum.server.api.request.post.JsonPostRequest;
+import xyz.kvantum.server.api.request.post.PostRequest;
+import xyz.kvantum.server.api.util.MapBuilder;
+import xyz.kvantum.server.api.views.rest.RestResponse;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+public final class UploadService extends RestResponse {
+
+    private static final JSONObject REQUEST_OF_WRONG_TYPE =
+        new JSONObject(MapBuilder.<String, Object>newHashMap().put("response", "request must be encoded using JSON").get());
+    private static final JSONObject REQUEST_MISSING_FILE_LIST =
+        new JSONObject(MapBuilder.<String, Object>newHashMap().put("response", "request must contain a file list").get());
+    private static final JSONObject REQUEST_MISSING_APPLICATION =
+        new JSONObject(MapBuilder.<String, Object>newHashMap().put("response", "request must contain a valid application reference").get());
+    private static final JSONObject REQUEST_FAILED_TO_STORE =
+        new JSONObject(MapBuilder.<String, Object>newHashMap().put("response", "failed to store paste").get());
+
+    private static final Map<String, Long> WRITE_THROTTLE = new ConcurrentHashMap<>();
+    private static final long WRITE_THROTTLE_TIMEOUT = TimeUnit.MINUTES.toMillis(5);
+
+    private static final Collection<String>
+        VALID_APPLICATIONS = Arrays.asList("plotsquared", "fastasyncworldedit", "incendopermissions", "kvantum");
+
+    @NotNull private static JSONObject missingFileContent(@NotNull final String fileName) {
+        return new JSONObject(MapBuilder.<String, Object>newHashMap().put("response",
+            String.format("Missing file content for file %s", fileName)).get());
+    }
+
+    private final File pasteFolder;
+
+    public UploadService(@NotNull final File pasteFolder) {
+        super(HttpMethod.POST, new ViewPattern("upload"));
+        this.pasteFolder = pasteFolder;
+        if (!pasteFolder.exists() && !pasteFolder.mkdir()) {
+            throw new IllegalArgumentException(String.format("Failed to create paste folder %s", pasteFolder.getName()));
+        }
+    }
+
+    @Override public JSONObject generate(@Nonnull AbstractRequest abstractRequest) {
+        if (WRITE_THROTTLE.containsKey(abstractRequest.getSocket().getIP())) {
+            final long lastWrite = WRITE_THROTTLE.get(abstractRequest.getSocket().getIP());
+            if ((System.currentTimeMillis() - lastWrite) < WRITE_THROTTLE_TIMEOUT) {
+                final long newWrite = WRITE_THROTTLE_TIMEOUT - ((System.currentTimeMillis()) - lastWrite);
+                final long newWriteMinutes = TimeUnit.MILLISECONDS.toMinutes(newWrite);
+                return new JSONObject(MapBuilder.<String, Object>newHashMap().put("response",
+                    String.format("you need to wait %d minutes before creating a new paste", newWriteMinutes)).get());
+            }
+        }
+        WRITE_THROTTLE.put(abstractRequest.getSocket().getIP(), System.currentTimeMillis());
+
+        final PostRequest request = abstractRequest.getPostRequest();
+        if (!(request instanceof JsonPostRequest)) {
+            return REQUEST_OF_WRONG_TYPE;
+        }
+        final JsonPostRequest jsonPostRequest = (JsonPostRequest) request;
+        if (!jsonPostRequest.contains("files") || jsonPostRequest.get("files") == null) {
+            return REQUEST_MISSING_FILE_LIST;
+        }
+        if (!jsonPostRequest.contains("paste_application") || jsonPostRequest.get("paste_application") == null) {
+            return REQUEST_MISSING_APPLICATION;
+        }
+        final String applicationId = jsonPostRequest.get("paste_application")
+            .toLowerCase(Locale.ENGLISH);
+        if (!VALID_APPLICATIONS.contains(applicationId)) {
+            return REQUEST_MISSING_APPLICATION;
+        }
+        final String[] files = jsonPostRequest.get("files").split(",");
+        final Map<String, String> fileMap = MapBuilder.<String, String>newHashMap().get();
+        final JSONArray fileNames = new JSONArray();
+        for (final String file : files) {
+            final String fileName = String.format("file-%s", file);
+            if (!jsonPostRequest.contains(fileName)) {
+                return missingFileContent(file);
+            }
+            fileMap.put(file, jsonPostRequest.get(fileName));
+            fileNames.add(file);
+        }
+
+        final JSONObject createdObject = new JSONObject();
+        createdObject.put("files", fileMap);
+        createdObject.put("file_names", fileNames);
+        createdObject.put("timestamp", System.currentTimeMillis());
+        createdObject.put("application_id", applicationId);
+
+        final String pasteId = UUID.randomUUID().toString().replaceAll("-", "");
+        // Store paste to database
+        final String rawString = createdObject.toJSONString();
+        final File file = new File(pasteFolder, String.format("%s.json", pasteId));
+        boolean created;
+        try {
+            created = file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            created = false;
+        }
+        if (!created) {
+            return REQUEST_FAILED_TO_STORE;
+        }
+        if (!file.canWrite()) {
+            if (!file.setWritable(true)) {
+                return REQUEST_FAILED_TO_STORE;
+            }
+        }
+        try {
+            Files.write(file.toPath(), rawString.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return REQUEST_FAILED_TO_STORE;
+        }
+
+        // Return response
+        final JSONObject response = new JSONObject();
+        response.put("created", createdObject);
+        response.put("paste_id", pasteId);
+        response.put("response", String.format("the paste can be viewed at https://incendo.org/paste/view/%s",
+            pasteId));
+        return response;
+    }
+
+}
